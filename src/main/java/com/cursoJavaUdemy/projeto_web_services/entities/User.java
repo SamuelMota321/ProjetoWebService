4c801001c905045d5de3de55414be0d97202923e
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-11-21 16:17:48 -0300

    user entity and resourse

---
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/User.java
@@ -0,0 +1,24 @@
+package com.cursoJavaUdemy.projeto_web_services.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
---
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/resourses/UserResource.java
@@ -0,0 +1,18 @@
+package com.cursoJavaUdemy.projeto_web_services.resourses;
+
+import com.cursoJavaUdemy.projeto_web_services.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "samuel", "samuel.mota.2010@gmail.com", "(61)99353-3322", "35221119");
+        return ResponseEntity.ok().body(user);
+    }
+}
