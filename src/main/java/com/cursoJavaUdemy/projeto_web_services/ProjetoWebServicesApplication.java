3b39a83edf637f895aa5ac3bd9d9532389e75acc
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-11-21 14:48:08 -0300

    Project created

---
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/ProjetoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJavaUdemy.projeto_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesApplication.class, args);
+	}
+
+}
