6debe0b5a6fdc361155e6d66a26d449574b968bc
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-11-22 12:49:36 -0300

    OrderStatus enum

--- src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/Order.java
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/Order.java
@@ -1,5 +1,6 @@
 package com.cursoJavaUdemy.projeto_web_services.entities;
 
+import com.cursoJavaUdemy.projeto_web_services.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
@@ -12,7 +13,6 @@ import java.time.Instant;
 @Setter
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "Orders")
 public class Order implements Serializable {
@@ -25,6 +25,7 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     // a notação Join sempre vai estar na tabela onde será colocada a chave estrangeira
     @ManyToOne
@@ -32,4 +33,19 @@ public class Order implements Serializable {
 //    @JsonIgnore // com o 'JsonIgnore' do lado do pedido o comportamento será trazer com o usuário os seus pedidos
     private User client;
 
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) this.orderStatus = orderStatus.getCode();
+
+    }
 }
