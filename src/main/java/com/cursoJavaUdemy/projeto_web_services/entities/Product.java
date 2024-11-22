40b1686ebb8c4adfbafc187bc4f6f2dc79a214f7
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-11-22 16:29:26 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/Product.java
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/Product.java
@@ -1,5 +1,6 @@
 package com.cursoJavaUdemy.projeto_web_services.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,8 +33,12 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "produto_categoria",
             joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id "))
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private final Set<Category> categories = new HashSet<>();
+
+    @JsonIgnore // aqui o json ignore teve que ser chamado duas vezes por causa da forma como é chamada a função na nova versão do JPA
+    @OneToMany(mappedBy = "id.product")
+    private final Set<OrderItem> items = new HashSet<>();
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
@@ -41,5 +46,14 @@ public class Product implements Serializable {
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
     }
 }
