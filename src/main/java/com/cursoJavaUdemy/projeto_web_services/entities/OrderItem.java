3fba0e4d1bac59b07098e1b9447b6c2aa6d57c72
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2024-11-22 15:15:33 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/cursoJavaUdemy/projeto_web_services/entities/OrderItem.java
@@ -0,0 +1,58 @@
+package com.cursoJavaUdemy.projeto_web_services.entities;
+
+import com.cursoJavaUdemy.projeto_web_services.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "order_item")
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    @Getter
+    @Setter
+    private Integer quantity;
+
+    @Getter
+    @Setter
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+}
